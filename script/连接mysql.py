1a42a569b585ea9cc9c45f4050d8071c62b195f2
zyb4950045 <1014945325@qq.com>
2018-01-30 19:55:31 +0800

    连接mysql测试

---
+++ script/连接mysql.py
@@ -0,0 +1,5 @@
+#coding=utf-8
+
+import mysql.connector
+
+print 'git success'
--- test.py
+++ test.py
@@ -1,3 +1,14 @@
 #coding=utf-8
 
-print 'git success'
+import mysql.connector
+
+user = 'root'
+passwd = None
+database = 'test'
+
+connection = mysql.connector.connect(user=user, password=passwd, database=database)
+cursor = connection.cursor()
+query = 'SELECT * FROM test'
+cursor.execute(query)
+for id, name in cursor:
+    print('id : %s, name : %s' %(id, name))
